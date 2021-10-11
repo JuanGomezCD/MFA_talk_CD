937f3f93e7febfff6313c1510b693f915b96f31f
Juan Gómez <juan.gomez@clouddistrict.com>
2021-10-11 13:47:19 +0200

    refactor controllers, and added decorators to EPs

--- mfa-poc/src/app.controller.ts
+++ mfa-poc/src/app.controller.ts
@@ -1,28 +1,11 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { AuthService } from './auth/auth.service';
+import { Controller, Get } from '@nestjs/common';
+import { Mfa, MfaLevels } from './auth/mfa.decorator';
 
-@Controller('auth')
+@Controller()
 export class AppController {
-  constructor(private readonly authService: AuthService) {}
-
-  @Get('/qr-code')
-  async getSecret(): Promise<string> {
-    const secret = this.authService.getSecret();
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const imgSrc = await this.authService.respondWithQRCode(secret.otpauthUrl);
-    return `<img src="${imgSrc}">`;
-  }
-
-  @Post('/login')
-  login(@Body('user') user: string, @Body('pass') pass: string): any {
-    const jwtToken = this.authService.logIn(user, pass);
-    return jwtToken;
-  }
-
-  @Post('/mfa/authenticate')
-  validateSecret(@Body('token') token: string): any {
-    const validated = this.authService.validateSecret(token);
-    return { validated: validated };
+  @Get()
+  @Mfa(MfaLevels.multiple)
+  getHello() {
+    return 'Hello world!';
   }
 }
--- mfa-poc/src/auth/auth.controller.ts
+++ mfa-poc/src/auth/auth.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Mfa, MfaLevels } from './mfa.decorator';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Get('/qr-code')
+  async getSecret(): Promise<string> {
+    const secret = this.authService.getSecret();
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const imgSrc = await this.authService.respondWithQRCode(secret.otpauthUrl);
+    return `<img src="${imgSrc}">`;
+  }
+
+  @Post('/login')
+  @Mfa(MfaLevels.public)
+  login(@Body('user') user: string, @Body('pass') pass: string): any {
+    const jwtToken = this.authService.logIn(user, pass);
+    return jwtToken;
+  }
+
+  @Post('/mfa/authenticate')
+  @Mfa(MfaLevels.single)
+  validateSecret(@Body('token') token: string): any {
+    const validated = this.authService.validateSecret(token);
+    return { validated: validated };
+  }
+}
